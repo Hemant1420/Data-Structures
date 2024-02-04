6a34d8f25f9999574bc8cc5238b9f85d1a9b1fdc
Softkorner <softKorner.ai@gmail.com>
2024-02-04 13:10:26 +0530

    Implemented Queue various Operations

---
+++ QueuePractice/Class1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuePractice
+{
+    public class Queue<T>
+    {
+        public T[] arr;
+        public int Front;
+        public int Rear;
+        public int Queuesize;
+        
+
+        public Queue(int size)
+        {
+            arr = new T[size];
+            Queuesize = size;
+            Front = -1;
+            Rear = -1;
+
+        }
+
+        
+
+        public bool Isfull()
+        {
+            if(Rear == Queuesize - 1)
+            {
+                Console.WriteLine("Queue is full");
+                return true;
+            }
+            return false;
+        }
+
+        public bool Isempty()
+        {
+            if(Front == -1 && Rear == -1) 
+            {
+                Console.WriteLine("Queue is empty");
+                return true;
+            }
+            return false;
+        }
+
+        public void Enqueue(T data)
+        {
+            if (Isfull())
+            {
+
+                Console.WriteLine("Queue is full");
+            }
+            else
+            {
+                if (Front == -1)
+                {
+                    Front = 0;
+                    Rear++;
+                    arr[Rear] = data;
+
+                }
+                else
+                {
+
+                    Rear++;
+                    arr[Rear] = data;
+                }
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (Isempty())
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                if (Front == Rear)
+                {
+                    Front = -1;
+                    Rear = -1;
+                }
+                else
+                {
+                    Front++;
+                }
+
+
+            }
+
+        }
+
+        public void Peek()
+        {
+            Console.WriteLine(arr[Front]);
+
+
+        }
+
+        public void Print()
+        {
+            for(int i = Front; i<=Rear; i++)
+            {
+                Console.Write($"{arr[i]}->");
+            }
+            Console.WriteLine();
+        }
+
+
+
+    }
+}
---
+++ QueuePractice/MainQueueTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuePractice
+{
+    internal class MainQueueTest
+    {
+        static void Main()
+        {
+            Queue<int> newQueue = new Queue<int>(5);
+            newQueue.Enqueue(1);
+            newQueue.Enqueue(2);
+            newQueue.Enqueue(3);
+            newQueue.Enqueue(4);
+            newQueue.Print();
+
+            newQueue.Dequeue();
+            newQueue.Print();
+
+
+            newQueue.Peek();
+        }
+    }
+}
