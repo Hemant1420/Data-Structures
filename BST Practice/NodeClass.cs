1ba5c7b77ac57d211b6a1724f7ec57a7bd4c9f13
Softkorner <softKorner.ai@gmail.com>
2024-02-05 08:01:35 +0530

    BST Assig 1

---
+++ BST Practice/NodeClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Practice
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Rightnode;
+        public Node<T> Leftnode;
+
+        public Node(T value)
+        {
+            Value = value;
+            Rightnode = Leftnode = null;
+
+
+        }
+
+
+
+    }
+}
---
+++ BST Practice/Operationcs.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Practice
+{
+    public class Operations<T> where T : IComparable<T>
+    {
+
+        Node<T> Root;
+
+        public Operations()
+        {
+            Root = null;
+        }
+
+        public void Insert(T value)
+        {
+
+            Node<T> newNode = new Node<T>(value);   
+
+            if (Root == null)
+            {
+                Root = newNode;
+                return;
+
+            }
+
+            Node<T> temp = Root;
+
+            while (true)
+            {
+
+                if (value.CompareTo(temp.Value) < 0)
+                {
+                    if (temp.Leftnode == null)
+                    {
+                        temp.Leftnode = null;
+                        return;
+                    }
+
+                    temp = temp.Leftnode;
+
+                }
+
+                else if (value.CompareTo(temp.Value) > 0)
+                {
+                    if (temp.Rightnode == null)
+                    {
+                        temp.Rightnode = newNode;
+                        return;
+                    }
+                    temp = temp.Rightnode;
+                }
+
+                else
+                {
+                    Console.WriteLine("The Value already exists");
+                }
+            }
+
+        }
+
+       /* public void print_InOrder(T value) 
+        {
+            Node<T> temp = Root;
+
+            
+            while(true)
+            if(temp.Leftnode != null)
+            {
+                Console.WriteLine($"{temp.Leftnode.Value}");
+                    temp = temp.Leftnode;
+            }
+
+        }*/
+
+    }
+}
+
+
+
+
+
+
+
+
