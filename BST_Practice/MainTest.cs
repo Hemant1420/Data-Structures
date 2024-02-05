dae7f00b9b12a86c6cbbadbe91a141000fa2cc5f
Softkorner <softKorner.ai@gmail.com>
2024-02-05 17:20:12 +0530

    Traversing through a BST without Recurssion

---
+++ BST_Practice/MainTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Practice
+{
+    public class MainTest
+    {
+        static void Main()
+        {
+            Operations<int> newOp = new Operations<int>();
+            newOp.Insert(10);
+            newOp.Insert(15);
+            newOp.Insert(8);
+            newOp.Insert(5);
+            newOp.Insert(4);
+            newOp.Insert(6);
+            newOp.Insert(18);
+            newOp.Insert(20);
+
+            
+        }
+
+    }
+}
---
+++ BST_Practice/Operationcs.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BST_Practice
+{
+    public class Operations<T> where T : IComparable<T>
+    {
+
+        public Node<T> Root;
+
+        public Operations()
+        {
+            Root = null;
+        }
+
+        public void Insert(T value)
+        {
+
+            Node<T> newNode = new Node<T>(value);   
+
+            if (Root == null)
+            {
+                Root = newNode;
+                return;
+
+            }
+
+            Node<T> temp = Root;
+
+            while (true)
+            {
+
+                if (value.CompareTo(temp.Value) < 0)
+                {
+                    if (temp.Leftnode == null)
+                    {
+                        temp.Leftnode = newNode;
+                        return;
+                    }
+
+                    temp = temp.Leftnode;
+
+                }
+
+                else if (value.CompareTo(temp.Value) > 0)
+                {
+                    if (temp.Rightnode == null)
+                    {
+                        temp.Rightnode = newNode;
+                        return;
+                    }
+                    temp = temp.Rightnode;
+                }
+
+                else
+                {
+                    Console.WriteLine("The Value already exists");
+                }
+            }
+
+        }
+
+       
+        
+
+
+    }
+}
+
+
+
+
+
+
+
+
